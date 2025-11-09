edfaa46f4d7c40428366e4276ab461e33f4273a2
kakafajar <kakafajar1511@gmail.com>
2025-11-09 20:24:06 +0700

    feat : menambahkan route admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\Landing;
+use App\Livewire\ProdukCrud;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Landing::class);
+Route::get('/admin', ProdukCrud::class)->name('admin');
